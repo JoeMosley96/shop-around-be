c3bcee779d06fa0e25bdb2aace7e72cbd20a7dee
root <root@Laptop>
2024-07-04 16:03:58 +0100

    create local stores endpoint

--- api/shopAround/api/urls.py
+++ api/shopAround/api/urls.py
@@ -11,10 +11,12 @@ router.register(r'prices', PriceReportViewSet)
 router.register(r'favourites', FavouriteProductsViewSet)
 router.register(r'users', UsersViewSet)
 
+
 register_converter(FloatConverter, 'float')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('', views.index, name="index"),
     path('price-report/<int:product_id>/<float:lat>/<float:lon>/<int:rad>/', views.price_report, name='price_report'),
+    path('stores/<float:lat>/<float:lon>/', views.local_stores, name='local_stores')
 ]
