5090dea7d43ae9a6a09f7c9da69da42491dde819
root <root@Laptop>
2024-07-08 14:07:46 +0100

    add error handling part 1

--- api/shopAround/shopAround/urls.py
+++ api/shopAround/shopAround/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+handler404 = error_404
+handler500 = error_500
