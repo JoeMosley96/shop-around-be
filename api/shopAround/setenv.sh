fa53759870df10ef6627df931247159af956b40a
root <root@Laptop>
2024-07-08 14:32:52 +0100

    adds error handling corrections

--- api/shopAround/setenv.sh
+++ api/shopAround/setenv.sh
@@ -2,7 +2,9 @@ if [ "$1" == "test" ]; then
     export DJANGO_ENV=testing
     psql -f create_db.sql
     echo "Environment set to testing"
-else
+elif [ "$1" == "production" ]; then
     export DJANGO_ENV=production
     echo "Environment set to production"
+else 
+    echo "Please use test or production"
 fi
