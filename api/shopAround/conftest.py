186393404b6eab7a79c1969600853309b447e45d
MikaGawin <mikasgawin@gmail.com>
2024-07-08 18:05:02 +0100

    bugfixes on testing

--- api/shopAround/conftest.py
+++ api/shopAround/conftest.py
@@ -1,8 +1,20 @@
 import pytest
 from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.db import connections
 
 @pytest.fixture(scope='function', autouse=True)
 def seed_test_database():
-    call_command('flush','--no-input')
-    call_command('seed','--no-input')
+    for conn in connections.all():
+        conn.close()
 
+    try:    
+        call_command('seed')
+
+    except OperationalError as e:
+        print(f"Eroor during setup: {e}")
+
+    yield
+
+    for conn in connections.all():
+        conn.close()
--- api/shopAround/test_shopAround_API.py
+++ api/shopAround/test_shopAround_API.py
@@ -1,37 +1,56 @@
 ENDPOINT = "http://127.0.0.1:8000/api/products/"
 
 import os
-from api.shopAround.conftest import 
 from django.urls import reverse
 from rest_framework.test import APIClient
 import pytest
 import requests
 
+# @pytest.fixture
+# def api_client():
+#     return APIClient()
+
 @pytest.mark.django_db
-def test_getRequest():
-    response = requests.get(ENDPOINT)
-    print(f"Recieved {len(response.json())} items")
-    assert len(response.json()) == 10
-    assert response.status_code == 200 
+def test_get_products(api_client):
+    url=reverse('api/products/')
+
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data[0] == {
+		"product_id": 1,
+		"product": "Apple",
+		"description": "Fresh red apples",
+		"brand": "FreshFarms",
+		"size": "1kg",
+		"product_photo_url": "http://example.com/apple.jpg",
+		"category": 1
+	}
+
+# @pytest.mark.django_db
+# def test_getRequest():
+#     response = requests.get(ENDPOINT)
+#     print(f"Recieved {len(response.json())} items")
+#     assert len(response.json()) == 10
+#     assert response.status_code == 200 
 
 # def getProducts():
 #     return requests.get(ENDPOINT)
 
-@pytest.mark.django_db
-def test_postProductRequest():
-    payload = {
-        "product": "test",
-        "description": "test",
-        "brand": "test",
-        "size": "test",
-        "product_photo_url": "test",
-        "category": 1
-    }
-    response = requests.post(ENDPOINT, json=payload)
-    print("response status code: ",response.status_code)
-    print("The following product has been created: ", response.json())
-    assert response.status_code == 201
-    # deleteRequest(response.json()["product_id"])
+# @pytest.mark.django_db
+# def test_postProductRequest():
+#     payload = {
+#         "product": "test",
+#         "description": "test",
+#         "brand": "test",
+#         "size": "test",
+#         "product_photo_url": "test",
+#         "category": 1
+#     }
+#     response = requests.post(ENDPOINT, json=payload)
+#     print("response status code: ",response.status_code)
+#     print("The following product has been created: ", response.json())
+#     assert response.status_code == 201
+#     # deleteRequest(response.json()["product_id"])
 
 
 # def createEntry():
